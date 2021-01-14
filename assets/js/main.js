501547bfa20558b6f1cf8113b0593c7cf47cccb4
Jonathan <Ancis@Mac-Jonathan.local>
2021-01-13 21:15:09 -0300

    sitio con pagina asistencia agregada

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$('#thPartido').addClass('text-warning');
+$('#thRepresent').addClass('text-warning');
+$('#thPctVotos').addClass('text-warning');
+
+$('#ttMas1').addClass('text-warning');
+$('#ttMas2').addClass('text-warning');
+$('#ttMas3').addClass('text-warning');
+
+$('#ttMenos1').addClass('text-warning');
+$('#ttMenos2').addClass('text-warning');
+$('#ttMenos3').addClass('text-warning');

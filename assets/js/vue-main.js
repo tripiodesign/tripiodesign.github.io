cc9fa243294ea25da5d76536e12c78ae6532963f
Jonathan <Ancis@Mac-Jonathan.local>
2021-01-21 20:13:51 -0300

    primeros usos de vue

---
+++ assets/js/vue-main.js
@@ -0,0 +1,11 @@
+var appIndex = new Vue({
+    el: '#appIndex',
+    data: {
+            titulo: 'ACERCA DE NOSOTROS',
+            parrafos:[
+                {p1: 'La apertura, la responsabilidad y la honestidad definen la transparencia del gobierno. En una sociedad libre, la transparencia es la obligación del gobierno de compartir información con los ciudadanos. Está en el centro de cómo los ciudadanos hacen responsables a sus funcionarios públicos. Aquí en TGIF creemos que el gobierno debe ser transparente.'},
+
+                {p2: 'Los gobiernos existen para servir a la gente. La información sobre cómo los funcionarios llevan a cabo los negocios públicos y gastan el dinero de los contribuyentes debe estar disponible y fácilmente entendida. Esta transparencia permite una gobernanza buena y justa. La transparencia promueve la rendición de cuentas y brinda información a los ciudadanos sobre lo que está haciendo su gobierno.'}
+            ],
+          },
+});
